0bb4d93a28c5aa90ead0f631fa48ae6f6e69ed35
AymanDevOps <aymanadevops@gmail.com>
2023-11-20 20:04:02 -0500

    Daily CodeWar

---
+++ 11.20.23.js
@@ -0,0 +1,16 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// solution
+
+//  opposite: This is the name of the function.
+// number =>: This part defines a function that takes one parameter, named number.
+// -number: The function returns the negation of the number parameter.
+
+opposite = number => -number
+  
